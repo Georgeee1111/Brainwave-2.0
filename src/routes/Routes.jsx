d6769d3d17a9c3a439c00ac28d0de4430782c135
Georgee O. Minguito <160137694+Georgeee1111@users.noreply.github.com>
2024-08-30 21:22:48 +0800

    Update Routes.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,7 +5,7 @@ import NotFound from "../components/generalComponents/NotFound";
 
 const RoutesComponent = () => {
   return (
-    <Router basename="/Brainwave-2.0">
+    <Router>
       <Routes>
         {homeRoutes.map((route, index) => (
           <Route key={index} path={route.path} element={route.element} />
